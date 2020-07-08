977cd8a7fcda727f887747e6fb0d2e4ff35aa15d
Leo <harrylwin57@gmail.com>
2020-07-08 17:14:06 +0630

    modified for errors

--- FNI-API/src/main/java/org/tat/fni/api/domain/services/PolicyDataService/LifePolicyService.java
+++ FNI-API/src/main/java/org/tat/fni/api/domain/services/PolicyDataService/LifePolicyService.java
@@ -129,6 +129,7 @@ public class LifePolicyService implements IPolicyDataService {
 					.agentCommission(lifePolicy.getAgentCommission()).date(start.toString())
 					.paid(!(i > paidTimes)).build();
 			remainingDates.add(remainingDate);
+			if (month == 0) break;
 			start = start.plusMonths(month);
 			i++;
 		}
